5007cc24a955028242fa7cc3d0af8400dd7c5f4f
Jaka Kranjc <lynxlynxlynx@sourcemage.org>
2015-03-02 21:14:56 +0100

    10pp: removed commented out code with no further uses

--- 10pp/extender.pl
+++ 10pp/extender.pl
@@ -109,31 +109,6 @@ sub extend {
 
         # final writeout of modified current block
         writeBlock ($output_handle, $trigger_half, $response_half);
-
-	# it would be ideal to drop this on a LINE
-	# loop, not block loop.
-#	if ($_ =~ m/^.*NumInParty\(\d\)$/) {
-#	    my $NumInParty_orig = $1;
-	    # calculate $NumInParty_change according to $party_num
-	    # and $NumInParty_orig, not sure, need lynx's opinion.
-#	    if ($1 <= 6) {
-#		my $NumInParty_change = $1 * 666;
-#	    }
-#	}
-#	if ($_ =~ m/[ ][ ]OR\(\d\)\n/)
-# # 	if ($_ =~ m/([ ][ ][ ][ ](.*)Player4(.*)\n)/) {
-# # 	    # might need less robust check for the ones that aren't together
-# # 	    # or random ordering support too, sigh, that'll get long.
-# # 	    if ($_ =~ m/($1[ ][ ][ ][ ](.*)Player5(.*)\n)/) {
-# # 		if ($_ =~ m/($1[ ][ ][ ][ ](.*)Player6(.*)\n)/) {
-# # 		    for (my $i =7; $i<$party_num; $i++) {
-# # 			s/$1/$2Player$i$3/ # dunno if I should do this with flag g or no
-# # 		    }
-# # 		}
-# # 	    }
-# # 	} # if player4
-
-	
     }
     close($output_handle);
     return 1;
