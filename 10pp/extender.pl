6346eed1c00adef981d0b81d5776e716a8acd8da
Jaka Kranjc <lynxlynxlynx@sourcemage.org>
2015-06-28 22:37:54 +0200

    10pp: removed haldamir exception (file is ignored anyway) + verified mod list

--- 10pp/extender.pl
+++ 10pp/extender.pl
@@ -8,7 +8,7 @@ use File::Basename;
 
 # files not to touch
 #my $exceptions = "cut35h.baf test31 uddrow16.d udlesa.d player1.dlg";
-my $exceptions = "cut35h.baf cut35.bcs test31 uddrow16.d udlesa.d udlesa.dlg uddrow16.dlg b!halj.d b!halj.dlg";
+my $exceptions = "cut35h.baf cut35.bcs test31 uddrow16.d udlesa.d udlesa.dlg uddrow16.dlg";
 
 # reads passed BAF and extends it to request max party count if needed
 sub extend {
