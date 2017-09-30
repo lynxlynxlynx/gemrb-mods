lynxlynxlynx/gemrb-mods

commit d5d664ba3c89e128e49812494d98d3de0ceb6586

10pp: expanded exlusion list, added one from haldamir

10pp/extender.pl
@@ -7,7 +7,8 @@ use warnings;
 use File::Basename;
 
 # files not to touch
-my $exceptions = "cut35h.baf test31 uddrow16.d udlesa.d";
+#my $exceptions = "cut35h.baf test31 uddrow16.d udlesa.d player1.dlg";
+my $exceptions = "cut35h.baf cut35.bcs test31 uddrow16.d udlesa.d udlesa.dlg uddrow16.dlg b!halj.d b!halj.dlg";
 
 # reads passed BAF and extends it to request max party count if needed
 sub extend {

commit 90b5f41f86be2dc97423a8934edf4bc2e0a7e0f5

extend: fixed one exception member
introduced already as bad in d5d664ba

10pp/extender.pl
@@ -8,7 +8,7 @@ use File::Basename;
 
 # files not to touch
 #my $exceptions = "cut35h.baf test31 uddrow16.d udlesa.d player1.dlg";
-my $exceptions = " cut35h.baf cut35.bcs test08 test31 uddrow16.d udlesa.d udlesa.dlg uddrow16.dlg fguard.bcs fguard.baf _fguard.bcs _fguard.baf ";
+my $exceptions = " cut35h.baf cut35h.bcs test08 test31 uddrow16.d udlesa.d udlesa.dlg uddrow16.dlg fguard.bcs fguard.baf _fguard.bcs _fguard.baf ";
 
 # reads passed BAF and extends it to request max party count if needed
 sub extend {